fd439ce0cddec99f92a8a08b30a748c003166a97
Shubham Maddhashiya <shubhamsipah00@gmail.com>
2021-04-11 23:25:16 +0530

    dropdown update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tempfile
 import torch
 import numpy as np
 from imageio import imread
+import os
 
 
 from DfpNet import TurbNetG 
@@ -49,7 +50,11 @@ st.sidebar.title('Deep Flow Prediction App')
 
 ux = st.sidebar.number_input('Enter Ux')
 uy = st.sidebar.number_input('Enter Uy')
-airfoil_type = st.sidebar.multiselect('Airfoil type', ['NACA 2412', 'NACA 2415'])
+
+airfoil_files = os.listdir('./airfoils')[:100]
+airfoil_files = [x.replace('.png', '') for x in airfoil_files]
+
+airfoil_type = st.sidebar.multiselect('Airfoil type', airfoil_files)
 
 
 st.sidebar.write('OR')
@@ -59,7 +64,10 @@ upload_file = st.sidebar.file_uploader('Choose file...')
 
 sts = st.sidebar.button('Submit')
 
+
+
 if sts:
+
     if upload_file:
         file = upload_file.read()
         tfile = tempfile.NamedTemporaryFile(delete=False) 
@@ -77,8 +85,16 @@ if sts:
         np_im = np.array(im)
         np_im = np_im / np.max(np_im)
 
-        st.markdown('## Airfoil')
-        st.image(np_im)
+
+        # c1, c2 = st.beta_columns(2)
+        # c1.header('Airfoil Geometry')
+        # print(np_im.shape)
+        # c1.image(np_im, use_column_width=True)
+        # c2.header('Lift-to-Drag Ratio')
+        # c2.write('0.37')
+
+        st.header('Lift-to-Drag Ratio is 0.37') 
+
         
         np_im = np.flipud(np_im).transpose() # in model's input format
 
@@ -95,10 +111,17 @@ if sts:
 
         resid = solver(np_arr, expo=5)
         print(resid)
+
+
+
         if resid == 1:
-            st.image('result_velX_pred.png')
-            st.image('result_velY_pred.png')
-            st.image('result_pressure_pred.png')
+            col1, col2, col3 = st.beta_columns(3)
+            col1.header('Velocity X')
+            col1.image('result_velX_pred.png', use_column_width=True)
+            col2.header('Velocity Y')
+            col2.image('result_velY_pred.png', use_column_width=True)
+            col3.header('Pressure')
+            col3.image('result_pressure_pred.png', use_column_width=True)
             st.balloons()
 
 
